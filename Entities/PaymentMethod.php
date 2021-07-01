1305c2ba8e7b7dcb0d674273e1f909a26145a139
haitham-mofleh-salla <haitham@salla.sa>
2021-07-02 01:34:02 +0400

    push cache

--- Entities/PaymentMethod.php
+++ Entities/PaymentMethod.php
@@ -2,13 +2,10 @@
 
 namespace Modules\PaymentMethodeModule\Entities;
 
-use App\Traits\ClearsResponseCache;
 use Illuminate\Database\Eloquent\Model;
 
 class PaymentMethod extends Model
-{
-    use ClearsResponseCache;
-    
+{   
     protected $fillable = [
         'name','display_name', 'country','min','max'
     ];
